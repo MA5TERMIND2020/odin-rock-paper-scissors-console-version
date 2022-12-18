0e0388b9757344103c7ccc6b4a6d6bcffcccf2f8
MA5TERMIND2020 <77905424+MA5TERMIND2020@users.noreply.github.com>
2022-12-17 19:38:07 -0500

    Create a function that will get the choice of the computer.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 //Create an array called choices that contains the three options that a player can choose from.
 const choices = ['rock', 'paper', 'scissors'];
 console.log(choices);
+
+//Create a function that will randomly select the computers choice from the array of choices.
+
+function getComputerChoice() {
+    const computerChoice = choices[Math.floor(Math.random(choices) * 3)];
+    console.log(computerChoice);
+    return computerChoice
+}
+
+getComputerChoice();
