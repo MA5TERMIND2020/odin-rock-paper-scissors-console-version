7d89a0fa781b5e9ce7819b42e8db34d018f811d8
MA5TERMIND2020 <77905424+MA5TERMIND2020@users.noreply.github.com>
2022-12-18 10:01:50 -0500

    Create a function that prompts the user for their  selection and validates their input so that only  case-insensitive matches for one of the options  listed in the choices array are accepted.

--- js/main.js
+++ js/main.js
@@ -9,5 +9,26 @@ function getComputerChoice() {
     console.log(computerChoice);
     return computerChoice
 }
-
 getComputerChoice();
+
+/*Create a function that will get the selection of the player. It will perform input validation
+ so that as long as the input is a case-insensitive match for one of the options that a player
+ can choose from it will be accepted. Otherwise, it will keep asking the user for their selection*/
+
+function getPlayerChoice() {
+    let validatedInput = false;
+    while(validatedInput == false) {
+        const choice = prompt("Please make your selection. You can choose either Rock, Paper, or Scissors.");
+        if (choice == null) {
+            continue;
+        }
+        const choiceInLower = choice.toLowerCase();
+        if (choices.includes(choiceInLower)) {
+            validatedInput = true;
+            console.log(choice);
+            return choiceInLower;
+        }
+    }
+}
+
+getPlayerChoice();
